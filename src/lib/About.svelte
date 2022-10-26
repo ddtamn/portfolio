3140e81924264374f4f96ddf5749615aa94f0084
diditamin.m <disanderlidsbun@gmail.com>
2022-10-27 01:10:17 +0800

    add avatar

---
+++ src/lib/About.svelte
@@ -0,0 +1,15 @@
+<section>
+    <div class="head">
+        <div class="title">About Me</div>
+        <div class="subtitle">My Introduction</div>
+        
+    </div>
+    
+</section>
+
+
+<style>
+    .head {
+        text-align: center;
+    }
+</style>
